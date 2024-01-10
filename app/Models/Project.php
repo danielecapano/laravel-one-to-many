8b33395006e055110c1612c855dc0c650b0401b9
danielecapano <daniele.capano@gmail.com>
2024-01-10 15:07:52 +0100

    ADD - Type Model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -14,6 +14,12 @@ class Project extends Model
         'preview',
         'slug',
         'description',
-        'creation_date'
+        'creation_date',
+        'type_id'
     ];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
